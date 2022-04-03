0c89924a2463d1224105b55da4ea10247850f534
SabbirSami <smd71430@gmail.com>
2022-04-03 22:25:48 +0600

    navber with custom link

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import CustomLink from "../CustomLink/CustomLink";
 
 const Header = () => {
     return (
         <div>
             <Navbar bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#home">Dream Bag</Navbar.Brand>
                     <Nav className="ms-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <CustomLink to="/">Home</CustomLink>
+                        <CustomLink to="/reviews">Reviews</CustomLink>
+                        <CustomLink to="/dashboard">Dashboard</CustomLink>
+                        <CustomLink to="/blogs">Blogs</CustomLink>
+                        <CustomLink to="/about">About</CustomLink>
                     </Nav>
                 </Container>
             </Navbar>
